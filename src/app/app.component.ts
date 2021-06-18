e838f376d85b2546c4b7623db212decf1f2a03b8
binarios-tz <zakaria.janah100@gmail.com>
2021-06-18 15:18:58 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { CountryService } from './countryservice';
 import { ProductService } from './productservice';
 import { Product } from './product';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SelectItemGroup } from "primeng/api";
+import { FilterService } from "primeng/api";
 
 interface City {
   name: string;
@@ -12,17 +14,15 @@ interface City {
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [CountryService, ProductService]
+  providers: [CountryService, ProductService, FilterService]
 })
 export class AppComponent {
-
-    registerForm: FormGroup;
-    submitted = false;
-
+  registerForm: FormGroup;
+  submitted = false;
 
   checked: boolean = true;
 
-  city: string='Chicago';
+  city: string = 'Chicago';
 
   username: string;
   password: string;
@@ -31,6 +31,8 @@ export class AppComponent {
   checked2: boolean = true;
 
   selectedCountryAdvanced: any[];
+
+  groupedCities: SelectItemGroup[];
   filteredCountries: any[];
   countries: any[];
   products: Product[];
@@ -41,7 +43,8 @@ export class AppComponent {
   constructor(
     private countryService: CountryService,
     private productService: ProductService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private filterService: FilterService
   ) {
     this.cities = [
       { name: 'New York', code: 'NY' },
@@ -56,28 +59,62 @@ export class AppComponent {
     this.countryService.getCountries().then(countries => {
       this.countries = countries;
     });
+    this.groupedCities = [
+      {
+        label: 'Germany',
+        value: 'de',
+        items: [
+          { label: 'Berlin', value: 'Berlin' },
+          { label: 'Frankfurt', value: 'Frankfurt' },
+          { label: 'Hamburg', value: 'Hamburg' },
+          { label: 'Munich', value: 'Munich' }
+        ]
+      },
+      {
+        label: 'USA',
+        value: 'us',
+        items: [
+          { label: 'Chicago', value: 'Chicago' },
+          { label: 'Los Angeles', value: 'Los Angeles' },
+          { label: 'New York', value: 'New York' },
+          { label: 'San Francisco', value: 'San Francisco' }
+        ]
+      },
+      {
+        label: 'Japan',
+        value: 'jp',
+        items: [
+          { label: 'Kyoto', value: 'Kyoto' },
+          { label: 'Osaka', value: 'Osaka' },
+          { label: 'Tokyo', value: 'Tokyo' },
+          { label: 'Yokohama', value: 'Yokohama' }
+        ]
+      }
+    ];
     this.productService.getProductsSmall().then(data => (this.products = data));
-     this.registerForm = this.formBuilder.group({
-            firstName: ['',  [Validators.required, Validators.minLength(6)]],
-            lastName: ['', [Validators.required, Validators.minLength(6)]],
-            email: ['', [Validators.required, Validators.email]],
-            password: ['', [Validators.required, Validators.minLength(6)]]
-        });
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.minLength(6)]],
+      lastName: ['', [Validators.required, Validators.minLength(6)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
-   // convenience getter for easy access to form fields
-    get f() { return this.registerForm.controls; }
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.registerForm.controls;
+  }
 
-    onSubmit() {
-        this.submitted = true;
+  onSubmit() {
+    this.submitted = true;
 
-        // stop here if form is invalid
-        if (this.registerForm.invalid) {
-            return;
-        }
-
-        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
     }
+
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value));
+  }
 
   filterCountry(event) {
     //in a real application, make a request to a remote url with the query and return filtered results, for demo we filter at client side
@@ -91,5 +128,28 @@ export class AppComponent {
     }
 
     this.filteredCountries = filtered;
+  }
+
+  filterGroupedCity(event) {
+    let query = event.query;
+    let filteredGroups = [];
+
+    for (let optgroup of this.groupedCities) {
+      let filteredSubOptions = this.filterService.filter(
+        optgroup.items,
+        ["label"],
+        query,
+        "contains"
+      );
+      if (filteredSubOptions && filteredSubOptions.length) {
+        filteredGroups.push({
+          label: optgroup.label,
+          value: optgroup.value,
+          items: filteredSubOptions
+        });
+      }
+    }
+
+    this.filteredGroups = filteredGroups;
   }
 }
